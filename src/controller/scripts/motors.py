cdc17dadb8b2d019c4677ae6fa6422c8ee811b41
Alex Rice <alexrice826@gmail.com>
2023-05-17 10:57:51 -0400

    motors

--- src/controller/scripts/motors.py
+++ src/controller/scripts/motors.py
@@ -8,38 +8,38 @@ from geometry_msgs.msg import Twist
 
 # GPIO.setmode(GPIO.BCM)
 
-# Motor1PWM = 20
-# Motor1Dir = 16
+Motor1PWM = 20
+Motor1Dir = 16
 # Motor2PWM = 26
 # Motor2Dir = 19
 # Motor3PWM = 1
 # Motor3Dir = 7
-# Motor4PWM = 5
-# Motor4Dir = 0
+Motor4PWM = 5
+Motor4Dir = 0
 
-# for motor in motors:
-#     GPIO.setup(motor, GPIO.OUT) #Initializes all pins as output
-#     print(f'Set {motor} as output')
-#     GPIO.output(motor, GPIO.LOW) #Sets default direction of motors
-#     print(f'Set {motor} to low')
+for motor in motors:
+    GPIO.setup(motor, GPIO.OUT) #Initializes all pins as output
+    print(f'Set {motor} as output')
+    GPIO.output(motor, GPIO.LOW) #Sets default direction of motors
+    print(f'Set {motor} to low')
 
-# pwm1 = GPIO.PWM(motors[0], 100)
-# pwm2 = GPIO.PWM(motors[2], 100)
-# pwm3 = GPIO.PWM(motors[4], 100)
-# pwm4 = GPIO.PWM(motors[6], 100)
-# print('set pwms')
+pwm1 = GPIO.PWM(motors[0], 100)
+pwm2 = GPIO.PWM(motors[2], 100)
+pwm3 = GPIO.PWM(motors[4], 100)
+pwm4 = GPIO.PWM(motors[6], 100)
+print('set pwms')
 
-# pwm1.start(0)
-# pwm2.start(0)
-# pwm3.start(0)
-# pwm4.start(0)
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+pwm4.start(0)
 
 def callback(data):
     print(data)
-    # pwm1.ChangeDutyCycle(data.linear)
+    pwm1.ChangeDutyCycle(data.linear)
     # pwm2.ChangeDutyCycle(data.linear)
     # pwm3.ChangeDutyCycle(data.linear)
-    # pwm4.ChangeDutyCycle(data.linear)
+    pwm4.ChangeDutyCycle(data.linear)
 
     
      
